3d6f2234ad82a1ad129c612f4e9ebe702d56e6ba
Popov Daniil <90310211+dlemmer@users.noreply.github.com>
2023-11-15 21:27:04 +0300

    created separate form django-handler

---
+++ FormHandler/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def form_page(request):
+    return render(request, 'form.html')
---
+++ static/js/form.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+
+    var form = document.querySelector(".form");
+    var trigger = document.querySelector(".trigger");
+    var closeButton = document.querySelector(".close-button");
+
+    function toggleModal() {
+        form.classList.toggle("show-form");
+    }
+
+    function windowOnClick(event) {
+        if (event.target === form) {
+            toggleModal();
+        }
+    }
+
+    trigger.addEventListener("click", toggleModal);
+    closeButton.addEventListener("click", toggleModal);
+    window.addEventListener("click", windowOnClick);
+
+});
+
