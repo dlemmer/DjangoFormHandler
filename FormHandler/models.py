ad512b757c409f97017233a5800758caf5964d1c
Popov Daniil <90310211+dlemmer@users.noreply.github.com>
2023-11-19 20:42:11 +0300

    edited models.py and DB cols

--- FormHandler/models.py
+++ FormHandler/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-    
+class User_data(models.Model):
+    name = models.CharField(max_length=15)
+    surname = models.CharField(max_length=15)
+    email = models.EmailField(max_length=50)
+    reg_data = models.DateField(auto_now_add=True)
+    reg_time = models.DateTimeField(auto_now_add=True, blank=True)
+    agreement_check = models.BooleanField()
--- FormHandler/views.py
+++ FormHandler/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from FormHandler.models import User_data
 from .forms import RegForm
 
 # Create your views here.
@@ -10,7 +12,11 @@ def form_page(request):
     else:
         form = RegForm(request.POST)
         if form.is_valid():
-            print (form.cleaned_data)
+            # print (form.cleaned_data)
+            try:
+                User_data.objects.create(**form.cleaned_data)
+            except:
+                form.add_error(None, 'Error sending data')
             
     data = {
         'form' : form
